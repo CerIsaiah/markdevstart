5b5e135b93a0f2d2fd8498b6db4e4ddcf8b6d5b0
Your <icisaiahcerven@gmail.com>
2024-08-02 01:26:50 -0500

    take 8

--- devmarketer_project/settings.py
+++ devmarketer_project/settings.py
@@ -140,17 +140,18 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
 # and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
-       os.path.join(BASE_DIR, 'react_build', 'static'),
+       os.path.join(BASE_DIR, 'react_build'),
    ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 '''
 else:
    STATIC_URL = '/static/'
    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
    STATICFILES_DIRS = [
-       os.path.join(BASE_DIR, 'react_build', 'static'),
+       os.path.join(BASE_DIR, react_build'),
    ]
    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 '''
